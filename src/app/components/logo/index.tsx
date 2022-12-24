8dbc9ccd85b4739cbbfc95bc59df47cc57ecc161
arkmadj <ahmadjinadu@gmail.com>
2022-12-24 13:42:17 +0100

    Added logo container

---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+import CarLogoImg from "../../../assets/images/car-logo.png";
+
+const LogoContainer = styled.div`
+	${tw`flex items-center `}
+`;
+
+const LogoText = styled.div`
+	${tw`m-1 text-xl font-bold text-black md:text-2xl`}
+`;
+
+const Image = styled.div`
+	width: auto;
+	${tw`h-6 md:h-9`}
+
+	img {
+		width: auto;
+		height: 100;
+	}
+`;
+
+export function Logo() {
+	return (
+		<LogoContainer>
+			<Image>
+				<img src={CarLogoImg} alt="Car logo" />
+			</Image>
+			<LogoText>MyCar.</LogoText>
+		</LogoContainer>
+	);
+}
