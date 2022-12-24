5486311664f1d6f43a0d4bbb594a793f3635f65f
arkmadj <ahmadjinadu@gmail.com>
2022-12-24 13:41:32 +0100

    Added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 import "./App.css";
+import { HomePage } from "./app/containers/HomePage";
+
+const AppContainer = styled.div`
+	${tw`flex flex-col w-full h-full`};
+`;
 
 function App() {
-	return <div className="App">Hello World!!!</div>;
+	return (
+		<AppContainer>
+			<HomePage />
+		</AppContainer>
+	);
 }
 
 export default App;
