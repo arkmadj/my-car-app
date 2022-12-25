642b6ab8b32f2d1deecaa985872fcb905d5a6acf
arkmadj <ahmadjinadu@gmail.com>
2022-12-25 23:48:29 +0100

    Addde outlined button

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -11,6 +11,8 @@ const BaseButton = styled.button`
   ${tw`px-5 py-3 m-1 text-xs font-semibold text-white transition-all duration-200 ease-in-out border-2 border-transparent border-solid rounded-md outline-none focus:outline-none`}
 `
 
-cn
+const OutlinedButton = styled(BaseButton)`
+  ${tw`bg-red-500 hover:bg-transparent hover:text-red-500 hover:border-red-500`}
+`
 
 export function Button(props: IButtonProps) {}
