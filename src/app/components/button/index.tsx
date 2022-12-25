e7722e1e8da7e82f377a611d9419b25bc7ff5883
arkmadj <ahmadjinadu@gmail.com>
2022-12-25 23:45:14 +0100

    Added base button

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 interface IButtonProps {
 	theme: "filled" | "outlined";
   text: string
 }
+
+const BaseButton = styled.button`
+  ${tw`px-5 py-3 m-1 text-xs font-semibold text-white transition-all duration-200 ease-in-out border-2 border-transparent border-solid rounded-md outline-none focus:outline-none`}
+`
+
+cn
 
 export function Button(props: IButtonProps) {}
