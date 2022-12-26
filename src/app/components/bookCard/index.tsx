3378a8e73d77e7fca49b6687b02d434939aded13
arkmadj <ahmadjinadu@gmail.com>
2022-12-26 23:24:01 +0100

    Added card container

---
+++ src/app/components/bookCard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const CardContainer = styled.div`
+  ${tw`flex items-center justify-center px-2 py-1 bg-white rounded-md`}
+`
