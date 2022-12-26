14a8225d670c65fdc5a128f34c9b21ce4fcef692
arkmadj <ahmadjinadu@gmail.com>
2022-12-26 23:29:05 +0100

    Added icon container

--- src/app/components/bookCard/index.tsx
+++ src/app/components/bookCard/index.tsx
@@ -3,13 +3,15 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const CardContainer = styled.div`
-  ${tw`flex items-center justify-center px-2 py-1 bg-white rounded-md md:px-6 md:py-2`}
-`
+	${tw`flex items-center justify-center px-2 py-1 bg-white rounded-md md:px-6 md:py-2`}
+`;
 
 const ItemContainer = styled.div`
-${tw}
+	${tw`flex`}
+`;
+
+const Icon = styled.div`
+  ${tw`mr-1 text-xs text-red-500 fill-current md:text-base md:mr-3`}
 `
 
-export function BookCard(){
-  
-}
+export function BookCard() {}
