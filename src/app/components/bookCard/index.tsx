3e5891041c87d63bde68e6c158acc3793f64a001
arkmadj <ahmadjinadu@gmail.com>
2022-12-26 23:26:51 +0100

    export bookCard

--- src/app/components/bookCard/index.tsx
+++ src/app/components/bookCard/index.tsx
@@ -3,5 +3,13 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const CardContainer = styled.div`
-  ${tw`flex items-center justify-center px-2 py-1 bg-white rounded-md`}
+  ${tw`flex items-center justify-center px-2 py-1 bg-white rounded-md md:px-6 md:py-2`}
 `
+
+const ItemContainer = styled.div`
+${tw}
+`
+
+export function BookCard(){
+  
+}
