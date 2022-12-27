53bc46b91e9631b3f5c042b98e602ea9cf7bfe4d
arkmadj <ahmadjinadu@gmail.com>
2022-12-27 15:14:43 +0100

    Added booking steps

---
+++ src/app/containers/HomePage/bookingSteps.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+  ${tw`flex flex-col items-center w-full py-3 lg:py-6`}
+`
+
+const Title = styled.h2`
+  ${tw`text-xl font-extrabold text-black lg:text-4xl`}
+`
+
+export function BookingSteps() {
+
+}
