f3609a613015c4dfbcf27b4007f86437b32af08e
arkmadj <ahmadjinadu@gmail.com>
2022-12-28 23:30:09 +0100

    Added car container

--- src/app/containers/HomePage/aboutUs.tsx
+++ src/app/containers/HomePage/aboutUs.tsx
@@ -6,4 +6,6 @@ const AboutUsContainer = styled.div`
   ${tw`flex flex-wrap items-center w-full py-4 bg-white 2xl:justify-center px-7 md:px-0`}
 `
 
+const CarContainer = styled
+
 export function AboutUs(){}
