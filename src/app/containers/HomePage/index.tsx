a4cbb2166ed61354c430737cf7eba45a370afeb2
arkmadj <ahmadjinadu@gmail.com>
2022-12-28 23:47:41 +0100

    Added about us section

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 import { BookCard } from "../../components/bookCard";
 import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navbar";
+import { AboutUs } from "./aboutUs";
 import { BookingSteps } from "./bookingSteps";
 import { TopSection } from "./topSection";
 
@@ -20,6 +21,8 @@ export function HomePage() {
       <BookCard/> 
       <Marginer direction="vertical" margin="5em"/>
       <BookingSteps/>
+      <Marginer direction="vertical" margin="5em"/>
+      <AboutUs/>
 		</PageContainer>
 	);
 }
