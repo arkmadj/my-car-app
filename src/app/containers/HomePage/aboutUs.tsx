5e5e9b7bf3ba7529d1e3846d1655b6f705d47d16
arkmadj <ahmadjinadu@gmail.com>
2022-12-28 23:39:36 +0100

    Added info text

--- src/app/containers/HomePage/aboutUs.tsx
+++ src/app/containers/HomePage/aboutUs.tsx
@@ -39,6 +39,6 @@ const Title = styled.h1`
   ${tw`text-2xl font-extrabold text-black md:text-5xl md:font-black md:leading-normal`}
 `
 
-const InfoText = styled.div``
+const InfoText = styled.div` `
 
 export function AboutUs(){}
