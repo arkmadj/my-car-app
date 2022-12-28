9d6a8e091a838bc24458df229be331e24faf69fe
arkmadj <ahmadjinadu@gmail.com>
2022-12-28 23:52:56 +0100

    Added mobile responsiveness

--- src/app/containers/HomePage/aboutUs.tsx
+++ src/app/containers/HomePage/aboutUs.tsx
@@ -6,7 +6,7 @@ import { SCREENS } from "../../components/responsive";
 import JeepImg from "../../../assets/images/jeep.png";
 
 const AboutUsContainer = styled.div`
-	${tw`flex flex-wrap items-center w-full py-4 bg-white 2xl:justify-center px-7 md:px-0`}
+	${tw`flex flex-wrap items-center justify-center w-full py-4 bg-white px-7 md:px-0`}
 `;
 
 const CardContainer = styled.div`
@@ -34,7 +34,7 @@ const CardContainer = styled.div`
 `;
 
 const InfoContainer = styled.div`
-	${tw`flex flex-col w-1/2 md:ml-6 2xl:ml-16`}
+	${tw`flex flex-col md:w-1/2 md:ml-6 2xl:ml-16`}
 `;
 
 const Title = styled.h1`
@@ -54,10 +54,15 @@ export function AboutUs() {
 			<InfoContainer>
 				<Title>Have the best experience with our rental deals</Title>
 				<InfoText>
-					We offer a wide variety of vehicles for you to choose from, including
-					sedans, SUVs, vans, and trucks. Our fleet is constantly being updated
-					with the latest models to ensure that you have the best driving
-					experience possible.
+					We are proud to offer a wide variety of vehicles for you to choose
+					from, including sedans, SUVs, vans, and trucks. Whether you need a
+					small car for a quick trip around town or a larger vehicle for a
+					longer road trip, we have something that will meet your needs. Our
+					fleet is constantly being updated with the latest models to ensure
+					that you have the best driving experience possible. All of our
+					vehicles are well-maintained and clean, and we offer a variety of
+					amenities, such as GPS navigation and satellite radio, to make your
+					trip even more enjoyable. 
 				</InfoText>
 			</InfoContainer>
 		</AboutUsContainer>
