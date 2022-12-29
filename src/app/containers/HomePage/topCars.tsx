2fa641fe6ff1a5113ae83f2675b1e0068cd19d66
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 00:44:26 +0100

    Added top cars

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -1,25 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-
+import { ICar } from "../../../typings/car";
+import { Car } from "../../components/car";
 
 const TopCarsContainer = styled.div`
-${tw`flex flex-col items-center justify-center w-full max-w-screen-lg px-4 mb-10 md:px-0`}
-`
+	${tw`flex flex-col items-center justify-center w-full max-w-screen-lg px-4 mb-10 md:px-0`}
+`;
 
 const Title = styled.h1`
-  ${tw`text-3xl font-extrabold text-black lg:text-5xl`}
-`
+	${tw`text-3xl font-extrabold text-black lg:text-5xl`}
+`;
 
 const CarsContainer = styled.div`
-  ${tw`flex flex-wrap justify-center w-full mt-7 md:mt-10`}
-`
+	${tw`flex flex-wrap justify-center w-full mt-7 md:mt-10`}
+`;
 
-export function TopCars(){
-  return (
-    <TopCarsContainer>
-      <Title>Explore our top deals</Title>
-      <CarsContainer>  </CarsContainer>
-    </TopCarsContainer>
-  )
+export function TopCars() {
+	const testCar: ICar = {
+		name: "Audi S3 Car",
+		mileage: "10k",
+		thumbnailSrc:
+			"https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+		dailyPrice: 70,
+		monthlyPrice: 1600,
+		gearType: "Auto",
+		gas: "Petrol",
+	};
+
+	const testCar2: ICar = {
+		name: "HONDA cITY 5 Seater Car",
+		mileage: "20k",
+		thumbnailSrc:
+			"https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+		dailyPrice: 50,
+		monthlyPrice: 1500,
+		gearType: "Auto",
+		gas: "Petrol",
+	};
+  
+	return (
+		<TopCarsContainer>
+			<Title>Explore our top deals</Title>
+			<CarsContainer>
+        <Car {...testCar}/>
+        <Car {...testCar2}/>
+        <Car {...testCar}/>
+      </CarsContainer>
+		</TopCarsContainer>
+	);
 }
