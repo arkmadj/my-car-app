1ee83bc8879e7f86a94f9fce9aaf2fe2fac7a2b7
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 00:32:42 +0100

    Added car component

--- src/app/components/car/index.tsx
+++ src/app/components/car/index.tsx
@@ -1,3 +1,5 @@
+import { faEllipsisH, faFillDrip, faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
@@ -74,5 +76,36 @@ const RentButton = styled(Button)`
 export function Car(props: ICarProps){
   const {name, thumbnailSrc, dailyPrice, monthlyPrice, mileage, gearType,gas} = props
 
-  return 
+  return <CarContainer>
+    <CarThumbnail>
+      <img src={thumbnailSrc} alt="Car"/>
+    </CarThumbnail>
+    <CarName>{name}</CarName>
+    <PriceContainer>
+      <DailyPrice>${dailyPrice}<SmallText>/day</SmallText></DailyPrice>
+      <MonthlyPrice>${monthlyPrice}<SmallText>/month</SmallText></MonthlyPrice>
+    </PriceContainer>
+    <Seperator/>
+    <CarDetailsContainer>
+      <CarDetail>
+        <SmallIcon>
+          <FontAwesomeIcon icon={faTachometerAlt}/>
+        </SmallIcon>
+        <CarInfo>{mileage}</CarInfo>
+      </CarDetail>
+      <CarDetail>
+        <SmallIcon>
+          <FontAwesomeIcon icon={faEllipsisH}/>
+        </SmallIcon>
+        <CarInfo>{gearType}</CarInfo>
+      </CarDetail>
+      <CarDetail>
+        <SmallIcon>
+          <FontAwesomeIcon icon={faFillDrip}/>
+        </SmallIcon>
+        <CarInfo>{gas}</CarInfo>
+      </CarDetail>
+    </CarDetailsContainer>
+    <RentButton text="Rent now"/>
+  </CarContainer>
 }
