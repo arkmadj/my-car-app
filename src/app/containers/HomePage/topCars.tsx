4ee7eb22b0a42e7dd3f21625d4278084c8833a0c
arkmadj <ahmadjinadu@gmail.com>
2022-12-29 23:17:53 +0100

    Added title

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -7,6 +7,10 @@ const TopCarsContainer = styled.div`
 ${tw`flex flex-col items-center justify-center w-full max-w-screen-lg px-4 mb-10 md:px-0`}
 `
 
+const Title = styled.h1`
+  ${tw`text-3xl font-extrabold text-black lg:text-5xl`}
+`
+
 export function TopCars(){
   return ()
 }
