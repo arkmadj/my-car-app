0d3658532699cbf59c319a816518cb445dbef576
arkmadj <ahmadjinadu@gmail.com>
2022-12-29 23:16:19 +0100

    Added top cars container

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 
 
-const TopCarsContainer = styled.div``
+const TopCarsContainer = styled.div`
+${tw`flex flex-col items-center justify-center w-full max-w-screen-lg px-4 mb-10 md:px-0`}
+`
 
 export function TopCars(){
   return ()
