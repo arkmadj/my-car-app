96e8f6bfe92686f29ded4a62aa3cc11be972c769
arkmadj <ahmadjinadu@gmail.com>
2022-12-29 23:37:08 +0100

    Added car container

--- src/app/components/car/index.tsx
+++ src/app/components/car/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 import { ICar } from "../../../typings/car";
 
 interface ICarProps extends ICar{}
 
-const CarContainer = styled.div``
+const CarContainer = styled.div`
+  ${tw`flex flex-col items-center p-3 pb-4 m-1 bg-white rounded-md sm:m-3 md:m-6`}
+`
 
 export function Car(props: ICarProps){
 
