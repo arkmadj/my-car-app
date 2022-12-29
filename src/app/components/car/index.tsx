c75b11b65934b000759edf58f41e9388cbc41793
arkmadj <ahmadjinadu@gmail.com>
2022-12-29 23:33:55 +0100

    Added card container

--- src/app/components/car/index.tsx
+++ src/app/components/car/index.tsx
@@ -1,1 +1,11 @@
 import React from "react";
+import styled from "styled-components";
+import { ICar } from "../../../typings/car";
+
+interface ICarProps extends ICar{}
+
+const CardContainer = styled.div``
+
+export function Car(props: ICarProps){
+
+}
