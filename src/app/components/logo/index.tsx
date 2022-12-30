237840dc2c0d77f8e750ffe1ca42a004bd3b6a63
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 23:51:04 +0100

    Added color prop to logo text

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -4,12 +4,17 @@ import tw from "twin.macro";
 
 import CarLogoImg from "../../../assets/images/car-logo.png";
 
+interface ILogoProps {
+	color?: "light" | "dark";
+}
+
 const LogoContainer = styled.div`
 	${tw`flex items-center `}
 `;
 
-const LogoText = styled.div`
+const LogoText = styled.div<{ color?: string }>`
 	${tw`m-1 text-xl font-bold text-black md:text-2xl`}
+	${({ color }) => (color === "white" ? tw`text-white` : tw`text-black`)}
 `;
 
 const Image = styled.div`
@@ -22,13 +27,15 @@ const Image = styled.div`
 	}
 `;
 
-export function Logo() {
+export function Logo(props: ILogoProps) {
+  const {color} = props;
+
 	return (
 		<LogoContainer>
 			<Image>
 				<img src={CarLogoImg} alt="Car logo" />
 			</Image>
-			<LogoText>MyCar.</LogoText>
+			<LogoText color={color || 'dark'}>MyCar.</LogoText>
 		</LogoContainer>
 	);
 }
