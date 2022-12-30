d568d99d8f46b4c0aa8c6979bb99d16c13d799a6
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 23:35:07 +0100

    Adjusted color and height

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -3,8 +3,9 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const FooterContainer = styled.div`
-  min-height: 20em;
-  ${tw`flex flex-wrap min-w-full pb-1 bg-blue-900 pt-7`}
+  min-height: 30em;
+  background: rgb(31 42 76);
+  ${tw`flex flex-wrap min-w-full pb-1 pt-7`}
 `
 
 export function Footer (){
