9bde2c40817c54df0d5df0f19c072c4ba1554ed3
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 23:31:20 +0100

    Added footer container

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const FooterContainer = styled.div`
+  min-height: 20em;
+  ${tw`flex flex-wrap min-w-full pb-1 bg-blue-900 pt-7`}
+`
+
+
+export function Footer (){
+  return
+}
