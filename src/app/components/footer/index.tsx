c3f7789334a5ff72479918930a55569a1cf057f7
arkmadj <ahmadjinadu@gmail.com>
2022-12-31 00:29:38 +0100

    Added link list

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -21,6 +21,10 @@ const AboutText = styled.p`
 	${tw`max-w-xs mt-5 text-sm font-normal leading-5 text-white`}
 `;
 
+const LinkList = styled.ul`
+  ${tw`flex flex-col list-none outline-none`}
+`
+
 export function Footer() {
 	return (
 		<FooterContainer>
