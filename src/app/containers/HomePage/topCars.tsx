20581a16e8f2af7829119bf87a20765d8baa496e
arkmadj <ahmadjinadu@gmail.com>
2022-12-30 01:31:58 +0100

    Added carousel

--- src/app/containers/HomePage/topCars.tsx
+++ src/app/containers/HomePage/topCars.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { ICar } from "../../../typings/car";
 import { Car } from "../../components/car";
+import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
 
 const TopCarsContainer = styled.div`
 	${tw`flex flex-col items-center justify-center w-full max-w-screen-lg px-4 mb-10 md:px-0`}
@@ -17,6 +19,8 @@ const CarsContainer = styled.div`
 `;
 
 export function TopCars() {
+	const [current, setCurrent] = useState(0);
+
 	const testCar: ICar = {
 		name: "Audi S3 Car",
 		mileage: "10k",
@@ -38,15 +42,60 @@ export function TopCars() {
 		gearType: "Auto",
 		gas: "Petrol",
 	};
-  
+
 	return (
 		<TopCarsContainer>
 			<Title>Explore our top deals</Title>
 			<CarsContainer>
-        <Car {...testCar}/>
+				<Carousel
+					value={current}
+					onChange={setCurrent}
+					slides={[
+						<Car {...testCar} />,
+						<Car {...testCar2} />,
+						<Car {...testCar} />,
+						<Car {...testCar2} />,
+						<Car {...testCar} />,
+						<Car {...testCar2} />,
+						<Car {...testCar} />,
+					]}
+					plugins={[
+						"clickToChange",
+						{
+							resolve: slidesToShowPlugin,
+							options: {
+								numberOfSlides: 3,
+							},
+						},
+					]}
+					breakpoints={{
+						640: {
+							plugins: [
+								{
+									resolve: slidesToShowPlugin,
+									options: {
+										numberOfSlides: 1,
+									},
+								},
+							],
+						},
+						900: {
+							plugins: [
+								{
+									resolve: slidesToShowPlugin,
+									options: {
+										numberOfSlides: 2,
+									},
+								},
+							],
+						},
+					}}
+				/>
+				<Dots value={current} onChange={setCurrent} number={2} />
+				{/* <Car {...testCar}/>
         <Car {...testCar2}/>
-        <Car {...testCar}/>
-      </CarsContainer>
+        <Car {...testCar}/> */}
+			</CarsContainer>
 		</TopCarsContainer>
 	);
 }
