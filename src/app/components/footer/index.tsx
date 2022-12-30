ad574f5dfb1a19bcdbf597b2d509a59d0a6a587b
arkmadj <ahmadjinadu@gmail.com>
2022-12-31 00:22:00 +0100

    Added inner container

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,19 +4,27 @@ import tw from "twin.macro";
 import { Logo } from "../logo";
 
 const FooterContainer = styled.div`
-  min-height: 30em;
-  background: #1D2124;
-  ${tw`flex flex-wrap min-w-full pb-1 pt-7`}
-`
+	min-height: 30em;
+	background: #1d2124;
+	${tw`flex items-center justify-center min-w-full pt-4 pb-1 md:pt-12`}
+`;
+
+const InnerContainer = styled.div`
+	${tw`flex flex-col w-full h-full max-w-screen-2xl`}
+`;
 
 const AboutContainer = styled.div`
-  ${tw`flex flex-col`}
-`
+	${tw`flex flex-col`}
+`;
 
-export function Footer (){
-  return <FooterContainer>
-    <AboutContainer>
-      <Logo color="light" bgColor="dark"/>
-    </AboutContainer>
-  </FooterContainer>
+export function Footer() {
+	return (
+		<FooterContainer>
+			<InnerContainer>
+				<AboutContainer>
+					<Logo color="light" bgColor="dark" />
+				</AboutContainer>
+			</InnerContainer>
+		</FooterContainer>
+	);
 }
