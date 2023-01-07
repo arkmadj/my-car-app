a50a83d2cf3e18d8eeab542b6226eb02a48f622d
arkmadj <ahmadjinadu@gmail.com>
2023-01-07 09:59:56 +0100

    add get all cars

--- src/app/services/carService/index.ts
+++ src/app/services/carService/index.ts
@@ -1,15 +1,20 @@
-// import { Query } from "../../../__generated__/graphql";
-// import { apolloClient } from "../../graphql";
-// import { GET_ALL_CARS } from "./queries";
+import { apolloClient } from "../../graphql";
+import { GET_ALL_CARS } from "./queries";
+import { GetCars_cars } from "./__generated__/GetCars";
 
 class CarService {
-	// public async getCars(): Promise<Query> {
-	// 	const response = await apolloClient
-	// 		.query({ query: GET_ALL_CARS as any })
-	// 		.catch((err) => {
-	// 			throw err;
-	// 		});
-	// }
+	public async getCars(): Promise<GetCars_cars[]> {
+		const response = await apolloClient
+			.query({ query: GET_ALL_CARS })
+			.catch((err) => {
+				throw err;
+			});
+
+		if (response && response.data) {
+			return response.data as GetCars_cars[];
+		}
+		return [];
+	}
 }
 
 export default new CarService();
